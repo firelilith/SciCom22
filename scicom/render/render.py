96b5a432088b611847b2be7931e221196ba49c0f
Spectre <ole@benstem.de>
2023-03-16 18:41:22 +0100

    ich hasse visualization ahh

--- scicom/render/render.py
+++ scicom/render/render.py
@@ -2,49 +2,20 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 import numpy as np
-import argparse
-from itertools import zip_longest
-from contextlib import ExitStack
+
+from typing import Iterable, Generator
 
 
-def load_files(data_files):
-    with ExitStack() as stack:
-        files = [stack.enter_context(open(f, "r")) for f in data_files]
-        for step in zip_longest(*files):
-            step = tuple(map(str.strip, step))
-            yield np.fromstring(" ".join(step), dtype=int, sep=" ").reshape(-1, 4).T
+def compare(series1: tuple[Generator[np.ndarray, None, None], np.ndarray[float], np.ndarray[float], np.ndarray[str]],
+            series2: tuple[Generator[np.ndarray, None, None], np.ndarray[float], np.ndarray[float], np.ndarray[str]],
+            *,
+            strict=True):
 
+    gen1, m1, t1, l1 = series1
+    gen2, m2, t2, l2 = series2
 
-def plot(files, dpi, out_file):
-    def update_figure(p, d):
-        p.set_data(d[0], d[1])
-        ax.set_xlim(0, 10)
-        ax.set_ylim(0, 10)
+    if strict:
+        if not (np.allclose(m1, m2) and np.all(l1 == l2)):
+            raise ValueError("strict mode enabled. masses and labels need to be identical")
 
-    data = load_files(files)
-
-    fig, ax = plt.subplots(1, 1)
-
-    l, = ax.plot([], [])
-
-    writer = animation.FFMpegWriter(fps=15)
-    with writer.saving(fig=fig, outfile=out_file, dpi=dpi):
-        for step in data:
-            update_figure(l, step)
-            writer.grab_frame()
-
-
-if __name__ == "__main__":
-    plot(["sample.txt"], 100, "out.mp4")
-
-    parser = argparse.ArgumentParser(prog="render engine for nbody simulation")
-
-    parser.add_argument("-s", "--style", type=str, required=False, default="./plotconfig.mplstyle",
-                        help="A .mplstyle file containing syle data")
-
-    args = parser.parse_args()
-
-    # plt.style.use(args.style)
-else:
-    # plt.style.use("./plotconfig.mplstyle")
-    pass
+    fig = plt.figure()
