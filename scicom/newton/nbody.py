wrong index in newtonian _diff_eq

scicom/newton/nbody.py
@@ -30,14 +30,27 @@ def _diff_eq(vals, m):
     dist_sca = distance_sca(vals[..., :3])
 
     with np.errstate(divide="ignore", invalid="ignore"):
-        mG = constants.G.value * m
-        mGdist = mG[:, None, None] * dist
-        acc = mGdist / dist_sca[:, :, None] ** 3
-        # acc = (constants.G.value * m[:, None, None] * dist /
-        #        (dist_sca ** 3)[:, :, None])
-
-    print(acc)
+        acc = (constants.G.value * m[None, :, None] * dist /
+               (dist_sca ** 3)[:, :, None])
 
     acc[~np.isfinite(acc)] = 0
     out[..., 3:] = np.sum(acc, axis=1)
+    return out
+
+
+def _naive(vals, m):
+    pos = vals[..., :3]
+    vel = vals[..., 3:]
+
+    out = np.zeros(vals.shape)
+    out[:, :3] = vel
+
+    acc = np.zeros(pos.shape)
+    for a in range(len(m)):
+        for b in range(len(m)):
+            if a == b:
+                continue
+            acc[a] += constants.G.value * m[b] * (pos[b] - pos[a]) / np.linalg.norm(pos[b] - pos[a]) ** 3
+
+    out[:, 3:] = acc
     return out

scicom/test.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 import newton
 import post_newton_eih
 import render.plotting
 import library.file_io as io
 from library import solar_system_reference
 from library import coords
+from newton.nbody import _diff_eq, _naive
 
 preset = io.load_yaml_preset("/home/spectre/PycharmProjects/SciCom22/scicom/examples/sun_earth.yml")
 # preset = solar_system_reference.solar_system()
@@ -13,19 +16,21 @@ x, v, m, names = preset
 # x -= coords.barycenter(x, m)
 # v -= coords.barycenter(v, m)
 
-print(coords.barycenter(x, m))
-
 newt = newton.nbody(*preset, 3600, 36000)
 post = post_newton_eih.nbody(*preset, 3600, 36000)
 
 gen1 = newt[0]
 gen2 = post[0]
 
-for g in gen1:
-    print("barycenter")
-    print(coords.barycenter(g[:, :3], m))
-    print("\\barycenter")
-
 # print([(g1 - g2) for g1, g2 in zip(gen1, gen2)])
 
 # render.plotting.compare(newt, post)
+
+x = np.array([[1, 0, 0], [0, 0, 0]])
+v = np.array([[5, 0, 0], [0, 0, -1]])
+m = np.array([1, 20]) * 1E9
+
+vals = np.concatenate((x, v), axis=1)
+
+print(_diff_eq(vals, m))
+print(_naive(vals, m))