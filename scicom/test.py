b15d373b0904f4354d0ca8caf5cd67b2a0619837
Spectre <ole@benstem.de>
2023-04-12 17:59:36 +0200

    stuff

--- scicom/test.py
+++ scicom/test.py
@@ -5,6 +5,7 @@ import newton
 import post_newton_eih
 import render.plotting
 from library import solar_system_reference
+from library.file_io import save_series
 from library import coords, file_io
 from render.renderlib import animate
 from render.plotting import *
@@ -23,8 +24,11 @@ tolerance = 10000000
 series1 = newton.adaptive_nbody(*preset, dt=1000, time=t, tolerance=tolerance)
 series2 = post_newton_eih.adaptive_nbody(*preset, dt=1000, time=t, tolerance=tolerance)
 
-ref = solar_system_reference.get_series(bodies=bodies, duration=t, dt=10000)
+# ref = solar_system_reference.get_series(bodies=bodies, duration=t, dt=10000)
 
-topdown_cmp(series1=series1, series1_name="newt",
-            series2=series2, series2_name="post",
-            show=True, radius=None)
+save_series("series/250y_newt.csv", series1)
+save_series("series/250y_post.csv", series2)
+
+#topdown_cmp(series1=series1, series1_name="newt",
+#            series2=series2, series2_name="post",
+#            show=True, radius=None)
